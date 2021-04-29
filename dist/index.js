5439d90e8070d58ed0952e222340ca526cc425b6
MuAnto <antonio.murolo@buzzoole.com>
2021-04-29 16:10:33 +0000

    deploy Thu Apr 29 2021 16:10:33 GMT+0000 (Coordinated Universal Time)

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+import style from './style.module.css.js';
+/* TEMPLATE HTML */
+
+const template = document.createElement("template");
+template.innerHTML =
+/*html*/
+`
+  <div class="${style.root}">I am a simple component with CONSTRUCTED Stylesheet</button>
+`;
+
+class SimpleComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.appendChild(template.content.cloneNode(true));
+  }
+
+}
+
+customElements.define("simple-component", SimpleComponent);
---
+++ dist/style.module.css.js
@@ -0,0 +1,8 @@
+export default {
+ "root": "_root_abznj_1"
+}
+const head = document.querySelector('head');
+const style = document.createElement('style');
+style.setAttribute('id', 'dist/style.module.css');
+style.appendChild(document.createTextNode("._root_abznj_1 { \n  color: red;\n  font-family: Arial;\n  font-size: 18px;\n}\n"));
+head.appendChild(style);
